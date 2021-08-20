558a77e756fd46f53a83e7fcd8ab11365165fee9
Patrick Jordan <ptj.bodega@gmail.com>
2021-08-19 22:04:36 -0500

    added comments to 01/main.cpp

--- 01-hello-world/main.cpp
+++ 01-hello-world/main.cpp
@@ -1,5 +1,12 @@
+//including libraries
 #include <iostream>
 
+//the main() function with {} <- marking the beggining and the end of it
 int main() {
+    /*
+    std = standard library | cout = console outpout 
+    the cout command is part of the std lib and it displays text to the terminal for the user to see
+    \n is the breakline short
+    */
     std::cout << "Hello World\n";
 }
